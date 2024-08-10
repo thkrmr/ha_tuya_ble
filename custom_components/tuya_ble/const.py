daae30432a986f621d062ac867c50a5338de03fc
Vincent Kramar <thkrmr@users.noreply.github.com>
2024-08-10 15:08:47 +0200

    Update const.py
    TUYA_API_FACTORY_INFO_URL changed as per the API Docs:
    https://developer.tuya.com/en/docs/archived-documents/75f8d057b8?id=Ka7kjx0i4m255

--- custom_components/tuya_ble/const.py
+++ custom_components/tuya_ble/const.py
@@ -30,7 +30,7 @@ SMARTLIFE_APP = "smartlife"
 TUYA_SMART_APP = "tuyaSmart"
 
 TUYA_API_DEVICES_URL: Final = "/v1.0/users/%s/devices"
-TUYA_API_FACTORY_INFO_URL: Final = "/v1.0/iot-03/devices/factory-infos?device_ids=%s"
+TUYA_API_FACTORY_INFO_URL: Final = "/v1.0/devices/factory-infos?device_ids=%s"
 TUYA_FACTORY_INFO_MAC: Final = "mac"
 
 BATTERY_STATE_LOW: Final = "low"
